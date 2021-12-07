e653d7bd5fed634641dfcad5a32b026cf7e522b3
Vlad <levkovv13@yandex.ru>
2021-12-07 14:29:02 +0300

    add host

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.19.192',]
+ALLOWED_HOSTS = ['51.250.19.192', ]
 
 
 # Application definition
